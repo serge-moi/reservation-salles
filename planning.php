c471fd5a2b3c3f2b267a198c85bd306f1e7618e1
serge <serge.moi@laplateforme.io>
2020-02-24 11:56:06 +0100

    Fin

--- planning.php
+++ planning.php
@@ -7,7 +7,7 @@ $connexion = getCo();
 $requete = "SELECT u.id, u.login,r.id, r.titre, r.description, r.debut, r.fin, r.id_utilisateur FROM utilisateurs AS u INNER JOIN reservations AS r WHERE u.id = r.id_utilisateur";
 $query = mysqli_query($connexion,$requete);
 $resultats = mysqli_fetch_all($query);
-var_dump($resultats);
+// var_dump($resultats);
 
 
 $jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
