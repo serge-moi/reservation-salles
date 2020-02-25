7fb446791920ff4aee6bce522aa91f03a35a7c1f
serge <serge.moi@laplateforme.io>
2020-02-25 13:59:52 +0100

    modif curdate week

--- planning.php
+++ planning.php
@@ -4,7 +4,7 @@ require_once("libraries/utilities.php");
 
 
 $connexion = getCo(); 
-$requete = "SELECT u.id, u.login,r.id, r.titre, r.description, r.debut, r.fin, r.id_utilisateur FROM utilisateurs AS u INNER JOIN reservations AS r WHERE u.id = r.id_utilisateur";
+$requete = "SELECT u.id, u.login,r.id, r.titre, r.description, r.debut, r.fin, r.id_utilisateur FROM utilisateurs AS u INNER JOIN reservations AS r WHERE u.id = r.id_utilisateur AND WEEK(debut) = WEEK(CURDATE())";
 $query = mysqli_query($connexion,$requete);
 $resultats = mysqli_fetch_all($query);
 // var_dump($resultats);
